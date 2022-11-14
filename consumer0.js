4578fdd594b01b17a0f190fb83035728720e9503
raulito012 <76129576+raulito012@users.noreply.github.com>
2022-11-14 19:19:37 -0400

    Update consumer0.js

--- consumer0.js
+++ consumer0.js
@@ -8,58 +8,58 @@ const rabbitSettings = [{
     password: 'guest',
     vhost: '/',
     authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
-
-},
-{
+},{
     protocol: 'amqp',
     hostname: 'localhost',
     port: 5673,
     username: 'guest',
     password: 'guest',
     vhost: '/',
-    authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
-
-}]
+    authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']}]
 
 connect();
 
 async function connect(){
 
-    const queue = "AP";
-    const Universidad = "UTESA"
+    const queue = "Tarea";
+    const Materia = "UTESA"
 
     for(i=0;i<rabbitSettings.length;i++){
-    try{
-        
-        const conn = await amqp.connect(rabbitSettings[i]);
-        console.log("Conexion creada...");
+        try{
 
-        const channel = await conn.createChannel();
-        console.log("Canal creado...");
+            const conn = await amqp.connect(rabbitSettings[i]);
+            console.log("Conexion creada...");
+    
+            const channel = await conn.createChannel();
+            console.log("Canal creado...");
+    
+            const res = await channel.assertQueue(queue);
+            console.log("Queue creado...");
+    
+            console.log('Esperando por mensajes from ${Materia}');
+            channel.consume(queue, message => {
+                let Tarea = JSON.parse(message.content.toString());
+                console.log('Tarea recibido ${Tarea.name}');
+                console.log(Tarea);
+    
+                if(Tarea.Materia == Materia){
+                    channel.ack(message);
+                    console.log("Mensaje borrado de la cola... \n");
+    
+                } else{
+                    console.log("Ese mensaje no es para mi, por lo tanto, no lo borrare.");
+                }
+    
+    
+            })
+    
+            break;
+        } catch(err){
+            console.error(`Error -> ${err}`);
+        }
 
-        const res = await channel.assertQueue(queue);
-        console.log("Queue creado...");
-
-        console.log('Esperando por mensajes de ${Universidad}');
-        channel.consume(queue, message => {
-            let AP = JSON.parse(message.content.toString());
-            console.log('AP recibido ${AP.name}');
-            console.log(Algoritmo);
-
-            if(AP.Universidad == Universidad){
-                channel.ack(message);
-                console.log("Mensaje borrado de la cola... \n");
-                
-            } else{
-                console.log("Ese mensaje no es para mi, por lo tanto, no lo borrare.");
-            }
-
-
-        })
-
-        break
-    } catch(err){
-        console.error('Error -> $(err)');
     }
-}
+
+
+
 }
