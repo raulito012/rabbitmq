41d8599d053767b18df59c9f47fb15cacde72b92
raulito012 <76129576+raulito012@users.noreply.github.com>
2022-11-13 22:16:53 -0400

    Update producer.js

--- producer.js
+++ producer.js
@@ -20,32 +20,44 @@ const rabbitSettings = [{
     authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
 
 }]
-
 connect();
 
 async function connect(){
 
     const queue = "Algoritmos";
+    const newQueue = "Carreras";
 
     const msgs = [
         {   "name":"Jose R Quezada Bueno", "Universidad":"UTESA"},
+
     ]
 
     try{
         
         const conn = await amqp.connect(rabbitSettings);
-        console.log("Conexion creada...");
+        console.log("Connection Created...");
 
         const channel = await conn.createChannel();
-        console.log("Canal creado...");
+        console.log("Channel Created...");
 
         const res = await channel.assertQueue(queue);
-        console.log("Cola creada...");
+        console.log("Queue Created...");
 
         for(let msg in msgs){
-            await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msgs[msg])));
+            await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg[msgs])));
             console.log('Mensaje enviado a la cola ${queue}');
         }
+
+        res = await channel.assertQueue(newQueue);
+        console.log("Queue Created...");
+
+        for(let msg in msgs){
+            await channel.sendToQueue(newQueue, Buffer.from(JSON.stringify(msgs[msg])));
+            console.log('Mensaje enviado a la cola ${newQueue}');
+        }
+
+        
+
 
     } catch(err){
         console.error('Error -> $(err)');
