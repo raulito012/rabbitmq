d245f85724e6b1567c7798579faa55a482298913
raulito012 <76129576+raulito012@users.noreply.github.com>
2022-11-14 19:21:06 -0400

    Update producer.js

--- producer.js
+++ producer.js
@@ -1,6 +1,6 @@
 const amqp = require("amqplib");
 
-const rabbitSettings = {
+const rabbitSettings = [{
     protocol: 'amqp',
     hostname: 'localhost',
     port: 5672,
@@ -8,44 +8,52 @@ const rabbitSettings = {
     password: 'guest',
     vhost: '/',
     authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
-
-}
+},{
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5673,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']}]
 
 connect();
 
 async function connect(){
 
-    const queue = "AP";
-    const newQueue = "Roles";
+    const queue = "Tarea";
+    const newQueue = "Materias";
 
     const msgs = [
-        {   "name":"Jose Quezada", "Universidad":"UTESA"},
-        {   "name":"Jose Quezada", "Ciudad":"Santiago"},
-        {   "name":"Jose Quezada", "Edad":"22"},
-        {   "name":"Jose Quezada", "Carrera":"Sistemas"},
+        {   "name":"Jose R. Quezada", "Universidad":"UTESA"},
+        {   "name":"Jose R. Quezada", "Profesor":"Ivan Mendoza"},
+        {   "name":"Jose R. Quezada", "Edad":"22"},
+        {   "name":"Jose R. Quezada", "Materia":"Algoritmo Paralelos"},
     ]
+
+    for(i=0;i<rabbitSettings.length;i++){
+        try{
+
+            const conn = await amqp.connect(rabbitSettings[i]);
+            console.log("Conexion creada...");
     
-     for(i=0;i<rabbitSettings.length;i++){
-    try{
-
-        const conn = await amqp.connect(rabbitSettings[i]);
-        console.log("Se ha creado la conexion...");
-
-        const channel = await conn.createChannel();
-        console.log("El canal ha sido creado...");
-
-        const res = await channel.assertQueue(queue);
-        console.log("La cola ha sido creada...");
-
-        for(let msg in msgs){
-            await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msgs[msg])));
-            console.log('Los mensajes se han enviado a la cola: ${queue}');
+            const channel = await conn.createChannel();
+            console.log("Conexion creada...");
+    
+            const res = await channel.assertQueue(queue);
+            console.log("Cola creada...");
+    
+            for(let msg in msgs){
+                await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msgs[msg])));
+                console.log('Mensaje enviado a la cola ${queue}');
+            }
+    
+    
+            break;
+        } catch(err){
+            console.error(`Error -> ${err}`);
         }
-
-
-
-    } catch(err){
-        console.error('Error -> $(err)');
     }
-}
+
+
 }
