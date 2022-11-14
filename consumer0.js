35397fecdd40172f6b215dad0501fc59b6d98623
raulito012 <76129576+raulito012@users.noreply.github.com>
2022-11-14 00:53:15 +0000

    Add files via upload

---
+++ consumer0.js
@@ -0,0 +1,54 @@
+const amqp = require("amqplib");
+
+const rabbitSettings = {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5673,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
+
+}
+
+connect();
+
+async function connect(){
+
+    const queue = "Algoritmos";
+    const Universidad = "UTESA"
+
+     
+    try{
+        
+        const conn = await amqp.connect(rabbitSettings);
+        console.log("Conexion creada...");
+
+        const channel = await conn.createChannel();
+        console.log("Canal creado...");
+
+        const res = await channel.assertQueue(queue);
+        console.log("Queue creado...");
+
+        console.log('Esperando por mensajes from ${Universidad}');
+        channel.consume(queue, message => {
+            let Algoritmo = JSON.parse(message.content.toString());
+            console.log('Algoritmo recibido ${Algoritmo.name}');
+            console.log(Algoritmo);
+
+            if(Algoritmo.Universidad == Universidad){
+                channel.ack(message);
+                console.log("Mensaje borrado de la cola... \n");
+                
+            } else{
+                console.log("Ese mensaje no es para mi, por lo tanto, no lo borrare.");
+            }
+
+
+        })
+
+
+    } catch(err){
+        console.error('Error -> $(err)');
+    }
+}
---
+++ producer.js
@@ -0,0 +1,58 @@
+const amqp = require("amqplib");
+
+const rabbitSettings = {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5672,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
+
+}
+
+connect();
+
+async function connect(){
+
+    const queue = "Algoritmos";
+    const newQueue = "Carreras";
+
+    const msgs = [
+        {   "name":"Jose R Quezada Bueno", "Universidad":"UTESA"},
+        {   "name":"Jose R Quezada Bueno", "Ciudad":"Santiago"},
+        {   "name":"Jose R Quezada Bueno", "Edad":"22"},
+        {   "name":"Jose R Quezada Bueno", "Carrera":"Sistemas"},
+    ]
+
+    try{
+        
+        const conn = await amqp.connect(rabbitSettings);
+        console.log("Connection Created...");
+
+        const channel = await conn.createChannel();
+        console.log("Channel Created...");
+
+        let res = await channel.assertQueue(queue);
+        console.log("Queue Created...");
+
+        for(let msg in msgs){
+            await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg[msgs])));
+            console.log('Mensaje enviado a la cola ${queue}');
+        }
+
+        res = await channel.assertQueue(newQueue);
+        console.log("Queue Created...");
+
+        for(let msg in msgs){
+            await channel.sendToQueue(newQueue, Buffer.from(JSON.stringify(msg[msgs])));
+            console.log('Mensaje enviado a la cola ${newQueue}');
+        }
+
+        
+
+
+    } catch(err){
+        console.error('Error -> $(err)');
+    }
+}
